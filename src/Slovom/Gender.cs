04504a87d17e4fff5fcc5cd8844ba455159fc423
IVO-LAPTOP\Ivo Malchev <ivo.malchev@gmail.com>
2019-03-25 08:25:11 +0200

    Writing code documentation;

--- src/Slovom/Gender.cs
+++ src/Slovom/Gender.cs
@@ -1,9 +1,21 @@
 ﻿namespace Slovom
 {
+    /// <summary>
+    /// Grammatical gender.
+    /// </summary>
     public enum Gender
     {
+        /// <summary>
+        /// Masculine gender
+        /// </summary>
         Male = 1,
+        /// <summary>
+        /// Feminine gender
+        /// </summary>
         Female = 2,
+        /// <summary>
+        /// Neuter gender
+        /// </summary>
         Neutral = 3
     }
 }
--- src/Slovom/INumberSpeller.cs
+++ src/Slovom/INumberSpeller.cs
@@ -1,9 +1,21 @@
 ﻿namespace Slovom
 {
+    /// <summary>
+    /// Provides abstraction to the core functionality of the library
+    /// </summary>
     public interface INumberSpeller
     {
+        /// <summary>
+        /// Checks if number can be spelled. If it can't <see cref="Spell(long, Gender)"/> will throw an exception.
+        /// </summary>
         bool CanSpell(long number);
 
+        /// <summary>
+        /// Spells number <paramref name="number"/> into written language words.
+        /// </summary>
+        /// <param name="number">The number to be spelled</param>
+        /// <param name="gender">Grammatical gender to spell in. If it does matter.</param>
+        /// <returns></returns>
         string Spell(long number, Gender gender = Gender.Neutral);
     }
 }
